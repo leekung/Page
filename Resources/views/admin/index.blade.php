6a6d0ec9eada762df0c3a5159c8a14e1dc133742
Leekung <grmark@gmail.com>
2016-06-29 07:51:46 +0700

    customize page index

--- Resources/views/admin/index.blade.php
+++ Resources/views/admin/index.blade.php
@@ -25,61 +25,54 @@
                 </div>
                 <!-- /.box-header -->
                 <div class="box-body">
-                    <table class="data-table table table-bordered table-hover">
-                        <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>{{ trans('page::pages.table.name') }}</th>
-                            <th>{{ trans('page::pages.table.slug') }}</th>
-                            <th>{{ trans('core::core.table.created at') }}</th>
-                            <th data-sortable="false">{{ trans('core::core.table.actions') }}</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <?php if (isset($pages)): ?>
-                        <?php foreach ($pages as $page): ?>
-                        <tr>
-                            <td>
-                                <a href="{{ URL::route('admin.page.page.edit', [$page->id]) }}">
-                                    {{ $page->id }}
-                                </a>
-                            </td>
-                            <td>
-                                <a href="{{ URL::route('admin.page.page.edit', [$page->id]) }}">
-                                    {!! isset($page->is_home) && (bool)$page->is_home == true ? '<i class="fa fa-home"></i>' : ''  !!}
-                                    {{ $page->title }}
-                                </a>
-                            </td>
-                            <td>
-                                <a href="{{ URL::route('admin.page.page.edit', [$page->id]) }}">
-                                    {{ $page->slug }}
-                                </a>
-                            </td>
-                            <td>
-                                <a href="{{ URL::route('admin.page.page.edit', [$page->id]) }}">
-                                    {{ $page->created_at }}
-                                </a>
-                            </td>
-                            <td>
-                                <div class="btn-group">
-                                    <a href="{{ URL::route('admin.page.page.edit', [$page->id]) }}" class="btn btn-default btn-flat"><i class="fa fa-pencil"></i></a>
-                                    <button data-toggle="modal" data-target="#modal-delete-confirmation" data-action-target="{{ route('admin.page.page.destroy', [$page->id]) }}" class="btn btn-danger btn-flat"><i class="fa fa-trash"></i></button>
+                    <div class="snp_navigation">
+                        <nav class="items snp_navigationInner">
+                            <div class="item hasSubmenu"><span>{{ trans('message.Company') }}</span></div>
+                            <nav class="items subNavigation" id="subNavigationCompany">
+                                <div class="item"><a href="{{ URL::route('admin.page.page.edit', 4) }}"><span>{{ trans('message.About S&P') }}</span></a>
                                 </div>
-                            </td>
-                        </tr>
-                        <?php endforeach; ?>
-                        <?php endif; ?>
-                        </tbody>
-                        <tfoot>
-                        <tr>
-                            <th>Id</th>
-                            <th>{{ trans('page::pages.table.name') }}</th>
-                            <th>{{ trans('page::pages.table.slug') }}</th>
-                            <th>{{ trans('core::core.table.created at') }}</th>
-                            <th>{{ trans('core::core.table.actions') }}</th>
-                        </tr>
-                        </tfoot>
-                    </table>
+                                <div class="item"><a href="{{ URL::route('admin.page.page.edit', 9) }}"><span>{{ trans('message.Learning Center') }}</span></a></div>
+                                <div class="item"><a href="{{ URL::route('admin.page.page.edit', 6) }}"><span>{{ trans('message.CSR') }}</span></a></div>
+                                <div class="item"><a href="{{ URL::route('admin.page.page.edit', 7) }}"><span>{{ trans('message.Career') }}</span></a></div>
+                                <div class="item hide"><a href=""><span>{{ trans('message.Contact us') }}</span></a></div>
+                            </nav>
+                            <div class="item hasSubmenu"><span>{{ trans('message.Product & Service') }}</span></div>
+                            <nav class="items subNavigation" id="subNavigationProduct">
+                                <div class="item"><a href="{{ URL::route('admin.page.page.edit', 2) }}"><span>{{ trans('message.Cake Gallery') }}</span></a></div>
+                                <div class="item">
+                                    <a href="{{ URL::route('admin.page.page.edit', 15) }}"><span>{{ trans('message.Bakery-Cake') }}</span></a>
+                                    | <a href="{{ URL::route('admin.page.page.edit', 16) }}"><span>{{ trans('message.Bakery-Bread') }}</span></a>
+                                    | <a href="{{ URL::route('admin.page.page.edit', 17) }}"><span>{{ trans('message.Bakery-MoonCake') }}</span></a>
+                                    | <a href="{{ URL::route('admin.page.page.edit', 18) }}"><span>{{ trans('message.Bakery-Pastry') }}</span></a>
+                                    | <a href="{{ URL::route('admin.page.page.edit', 19) }}"><span>{{ trans('message.Bakery-Cookies') }}</span></a>
+
+                                </div>
+                                <div class="item">
+                                    <a href="{{ URL::route('admin.page.page.edit', 14) }}"><span>{{ trans('message.Restaurant-Breakfast') }}</span></a>
+                                    | <a href="{{ URL::route('admin.page.page.edit', 13) }}"><span>{{ trans('message.Restaurant-Highlight Menu') }}</span></a>
+                                    | <a href="{{ URL::route('admin.page.page.edit', 12) }}"><span>{{ trans('message.Restaurant-Healthy Meal') }}</span></a>
+                                    | <a href="{{ URL::route('admin.page.page.edit', 11) }}"><span>{{ trans('message.Restaurant-Beverage & Dessert') }}</span></a>
+                                </div>
+                                <div class="item">
+                                    <a href="{{ URL::route('admin.page.page.edit', 21) }}"><span>{{ trans('message.Coffee-Espresso Hot') }}</span></a>
+                                    | <a href="{{ URL::route('admin.page.page.edit', 22) }}"><span>{{ trans('message.Coffee-Iced Espresso') }}</span></a>
+                                    | <a href="{{ URL::route('admin.page.page.edit', 20) }}"><span>{{ trans('message.Coffee-Espresso Frost') }}</span></a>
+                                </div>
+                                <div class="item"><a href="#"><span>{{ trans('message.Package Food') }}</span></a></div>
+                                <div class="item"><a href="#"><span>{{ trans('message.Caterman') }}</span></a></div>
+                            </nav>
+                            <div class="item"><a href="#"><span>{{ trans('message.Investor Relations') }}</span></a></div>
+                            <div class="item hide"><a href=""><span>{{ trans('message.Promotion') }}</span></a></div>
+                            <div class="item"><a href="{{ URL::route('admin.page.page.edit', 3) }}"><span>{{ trans('message.Joy Card') }}</span></a></div>
+                            <div class="item hide"><a href="#"><span>{{ trans('message.News & Activity') }}</span></a></div>
+                            <div class="item"><a href="{{ URL::route('admin.page.page.edit', 5) }}"><span>{{ trans('message.Store Locations') }}</span></a></div>
+                            <hr>
+                            <div class="item"><a href="{{ URL::route('admin.page.page.edit', 8) }}"><span>{{ trans('message.FAQ') }}</span></a></div>
+                            <div class="item"><a href="{{ URL::route('admin.page.page.edit', 10) }}"><span>{{ trans('message.Sitemap') }}</span></a></div>
+                            <div class="item"><a href="#"><span>{{ trans('S&P Magazine') }}</span></a></div>
+                        </nav>
+                    </div>
+
                     <!-- /.box-body -->
                 </div>
                 <!-- /.box -->
@@ -87,6 +80,26 @@
         </div>
     </div>
     @include('core::partials.delete-modal')
+@stop
+
+@section('styles')
+<style type="text/css">
+    .snp_navigation .hasSubmenu {
+        font-weight:bold;
+    }
+    .snp_navigation .items.subNavigation {
+        margin-left: 20px;
+    }
+    .snp_navigation .item {
+        line-height: 2.5em;
+    }
+    .snp_navigation .item > a {
+        padding: 5px;
+    }
+    .snp_navigation .item > a:hover {
+        border-bottom: solid 2px #3c8dbc;
+    }
+</style>
 @stop
 
 @section('footer')
